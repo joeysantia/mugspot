ffc961f8465a5aed617e23f32f4a38775ecc19f7
Joey Santia <joey.santia@gmail.com>
2023-03-30 09:45:19 -0400

    Integrate Firebase, edit Home component spacing

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCYDMelFC9-dPakiurR6gi_RsSR6wCvDuQ",
+  authDomain: "mugspot-4d038.firebaseapp.com",
+  projectId: "mugspot-4d038",
+  storageBucket: "mugspot-4d038.appspot.com",
+  messagingSenderId: "92318564577",
+  appId: "1:92318564577:web:9122aeefa6fe4d04aeb7a0",
+  measurementId: "G-1D6CGFZM16"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
